4101b86de8381a14cc20fecc86968f3ac33aaecc
lVeliarl <dimalitx@gmail.com>
2019-03-21 14:11:18 +0300

    feat: partial solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  function getPrimeFactors (num) {
+    let primeFactors = [];
+    while (num % 2 === 0) {
+      primeFactors.push(2);
+      num = num / 2;
+    }
+    let sqrtNum = Math.sqrt(num);
+    for (let i = 3; i <= sqrtNum; i++) {
+      while (num % i === 0) {
+        primeFactors.push(i);
+        num = num / i;
+      }
+    }
+    if (num > 2) {
+      primeFactors.push(num);
+    }
+    return primeFactors;
+  }
+  let prime = Math.max(...getPrimeFactors(base));
+  let count = 0;
+  for (let k = prime; number / k >= 1; k *= prime) {
+    count = count + Math.floor(number / k);
+  }
+  return count;
 }
